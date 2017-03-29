502faabb370f66c483c324d86b10f952689e2ab1
chinmaykulkarni <chinmay.kulkarni333@gmail.com>
2017-03-30 03:59:42 +0530

    cozalozawoza program initialize

---
+++ src/learn/basic/homework/CozaLozaWoza.java
@@ -0,0 +1,17 @@
+package learn.basic.homework;
+//Write a program called CozaLozaWoza which prints the numbers 1 to 110, 11 numbers per line. The program shall print "Coza" in place of the numbers which are multiples of 3, "Loza" for multiples of 5, "Woza" for multiples of 7, "CozaLoza" for multiples of 3 and 5, and so on. The output shall look like:
+//1 2 Coza 4 Loza Coza Woza 8 Coza Loza 11 
+//Coza 13 Woza CozaLoza 16 17 Coza 19 Loza CozaWoza 22 
+//23 Coza Loza 26 Coza Woza 29 CozaLoza 31 32 Coza
+public class CozaLozaWoza {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	public void cozalozawoza(int i){
+		
+	}
+
+}
