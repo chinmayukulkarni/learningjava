3a643aacc56b5697d1a4bb5fea6a66e9207425bd
chinmaykulkarni <chinmay.kulkarni333@gmail.com>
2017-03-30 04:14:09 +0530

    CozaLozaWoza program run successfully

--- src/learn/basic/homework/CozaLozaWoza.java
+++ src/learn/basic/homework/CozaLozaWoza.java
@@ -6,12 +6,28 @@ package learn.basic.homework;
 public class CozaLozaWoza {
 
 	public static void main(String[] args) {
-		
+		CozaLozaWoza clz = new CozaLozaWoza();
+		clz.cozalozawoza();
 
 	}
 	
-	public void cozalozawoza(int i){
-		
+	public void cozalozawoza(){
+		for(int i=1;i<=110;i++){
+			if(i%15==0){
+				System.out.print(" CozaLoza ");
+			}else if(i%3==0){
+				System.out.print(" Coza ");
+			}else if(i%5==0){
+				System.out.print(" Loza ");
+			}else if(i%7==0){
+				System.out.print(" Woza ");
+			}else{
+                System.out.print(" "+i);
+            }
+            if(i%11==0){
+            System.out.println("");
+            }
+		}
 	}
 
 }
