5a99e3dc01c3c82fa866a1359315179aa5d078e5
chinmaykulkarni <chinmay.kulkarni333@gmail.com>
2017-03-30 05:43:08 +0530

    New Class files Added

---
+++ src/learn/basic/homework/Temp.java
@@ -0,0 +1,9 @@
+package learn.basic.homework;
+
+public class Temp {
+
+	public static void main(String[] args) {
+		System.out.println("Hi this is temp");
+	}
+
+}
---
+++ src/learn/basic/homework/Temp2.java
@@ -0,0 +1,10 @@
+package learn.basic.homework;
+
+public class Temp2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hi this is temp2");
+
+	}
+
+}
