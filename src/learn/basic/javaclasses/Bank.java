056f9119021bfd8ce30e461fa7fd642bf6ead814
chinmaykulkarni <chinmay.kulkarni333@gmail.com>
2017-04-02 10:26:20 +0530

    classes

---
+++ src/learn/basic/javaclasses/Bank.java
@@ -0,0 +1,32 @@
+package learn.basic.javaclasses;
+
+public class Bank {
+	String customerName;
+	int customerAccno;
+	int customerDOB;
+	String customerAddress;
+	String customerPAN;
+	int customerAdhar;
+
+	public void openAccountInBank() {
+	}
+
+	public void transferMoneyFromBank() {
+	}
+
+	public void createFixDepositInBank() {
+	}
+
+	public void loanFromBank() {
+	}
+
+	public void insuranceInBank() {
+	}
+
+	public void depoditMoney() {
+	}
+
+	public void withdrawMoney() {
+	}
+
+}
---
+++ src/learn/basic/javaclasses/Computer.java
@@ -0,0 +1,15 @@
+package learn.basic.javaclasses;
+
+public class Computer {
+
+	int screensize;
+	int prize;
+	String operatingsystem;
+	String harddisktype;
+	int ramsize;
+	
+	public void coolComputer(){}
+	public void startComputer(){}
+	public void openApplication(){}
+	
+}
