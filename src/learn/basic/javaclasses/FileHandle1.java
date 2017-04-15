e694d878555a8890081bcda6ac14742e829e514e
Chinmay <Chinmay@192.168.1.106>
2017-04-15 19:49:31 +0530

    file handlemain file

--- src/learn/basic/javaclasses/FileHandle1.java
+++ src/learn/basic/javaclasses/FileHandle1.java
@@ -3,7 +3,6 @@ package learn.basic.javaclasses;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
