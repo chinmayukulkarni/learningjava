f59e07698671651781059bc0b6668cf2d69d8a70
Chinmay <Chinmay@192.168.1.106>
2017-04-15 22:40:47 +0530

    original file for read and write

--- src/learn/basic/javaclasses/FileHandle1.java
+++ src/learn/basic/javaclasses/FileHandle1.java
@@ -15,7 +15,7 @@ public class FileHandle1 {
 
 		FileHandle1 fh = new FileHandle1();
 
-		String filename = "temp1.txt";
+		String filename = "temp.txt";
 		fh.read(filename);
 		fh.writeFile("temp1.txt", "this is new data in file");
 	}
