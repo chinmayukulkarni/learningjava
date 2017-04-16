8c26cb68430e2b27254be3a6fa0c5e032387ee72
chinmaykulkarni <chinmay.kulkarni333@gmail.com>
2017-04-16 22:51:17 +0530

    Bank interFace added

---
+++ src/learn/basic/javaclasses/BankInterface.java
@@ -0,0 +1,30 @@
+package learn.basic.javaclasses;
+
+import java.io.IOException;
+
+public interface BankInterface {
+	public String createAccountInBank(String name, String dob, String addes, String pan, String adhar, int money);
+
+	public Customer[] getAllCustomersInBank();
+
+	public Customer updateAccountFromAccountNo(String acno, String name, String addes, String dob, String pan,
+			String adhar);
+
+	public Customer[] deleteAccount(String delCust);
+
+	public Customer depositMoney(int amount, String accountNumber);
+
+	public Customer withdrawMoney(int amount, String accountNumber);
+
+	public Customer getAccountFromAccountNumber(String AccountNo1);
+
+	public void printAll();
+
+	public void writeFile() throws IOException;
+
+	public void printOneAcc(String oneCustomer);
+
+	public void readFile() throws IOException;
+	
+
+}
